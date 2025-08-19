be0ad6b5cd02d77cd438a0b5d690b2a5a9721e6d
Kelly <88363184+Waxomoly@users.noreply.github.com>
2025-08-19 16:15:08 +0700

    feat: kerangka login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { PUBLIC_API_BASE } from '$env/static/public';
+  function login() {
+    const base = PUBLIC_API_BASE || '';
+    window.location.href = `${base}/api/auth/google`;
+  }
+</script>
+
+<section class="w-full h-[89vh] flex items-center justify-center">
+  <button on:click={login} class=" flex items-center">
+    <img src="http://pluspng.com/img-png/google-logo-png-open-2000.png" alt="" class="h-[50px] mr-4">
+    <p class="text-2xl">Sign in with Google</p>
+    
+  </button>
+</section>
