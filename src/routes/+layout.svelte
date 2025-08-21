4877bc75702f8f6beab5b46bddc8a7ba7f4b9a7e
ChristopherJ123 <christopherz2002@hotmail.com>
2025-08-22 00:54:18 +0700

    refactor: add fonts, remove unused styles and header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,18 @@
 <script>
-  import '../app.css';
+  import "../app.css";
 </script>
 
-<header>
-  <nav>
-    <div>
-      <a href="/">OpenHouse 2025</a>
-    </div>
-    <div>
-      <a href="/ukm">UKM</a>
-    </div>
-  </nav>
-</header>
+<!--<header>-->
+<!--  <nav>-->
+<!--    <div>-->
+<!--      <a href="/">OpenHouse 2025</a>-->
+<!--    </div>-->
+<!--    <div>-->
+<!--      <a href="/ukm">UKM</a>-->
+<!--    </div>-->
+<!--  </nav>-->
+<!--</header>-->
 
 <main>
   <slot />
 </main>
-
