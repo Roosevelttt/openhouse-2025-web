b3f6b809f01987b6384a4680b1f583f95e6edfb7
Kelly <88363184+Waxomoly@users.noreply.github.com>
2025-09-05 00:19:23 +0700

    feat: login page styling

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import { PUBLIC_API_BASE } from '$env/static/public';
   import { page } from '$app/stores';
-  import Swal from '$lib/components/swal/swal.svelte'
+  import Swal from '$lib/components/swal/swal.svelte';
+  import CarnivalTableBg from '$lib/components/background/CarnivalTable.svelte';
 
   function login() {
     const base = PUBLIC_API_BASE || '';
@@ -45,16 +46,22 @@
 
 <Swal />
 
-<section class="w-full h-[89vh] flex items-center justify-center flex-col">
-  {#if errorMessage}
+<CarnivalTableBg class="min-h-screen !w-screen flex items-center justify-center z-30">  
+
+<section class="z-[999] min-h-screen flex items-center justify-center flex-col relative">
+
+  <!-- {#if errorMessage}
     <div class="mb-4 p-4 bg-red-100 border border-red-400 text-red-700 rounded">
       {errorMessage}
     </div>
-  {/if}
+  {/if} -->
   
-  <button on:click={login} class=" flex items-center">
+  <button on:click={login} class="drop-shadow-lg font-[family-name:Lexend] hover:bg-[var(--color-orange-center)] bg-[var(--color-orange-dark)] hover:cursor-pointer flex items-center py-3 px-5 rounded-full">
     <img src="http://pluspng.com/img-png/google-logo-png-open-2000.png" alt="" class="h-[50px] mr-4">
-    <p class="text-2xl">Sign in with Google</p>
+    <p class="text-2xl text-[var(--text-primary)]">Sign in with Google</p>
     
   </button>
+  <p class="mt-2 font-[family-name:Lexend] text-[var(--text-primary)]">* use your JOHN email</p>
 </section>
+
+</CarnivalTableBg>  
