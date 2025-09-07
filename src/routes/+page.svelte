3321a61c0801fde9bda9e1966815dfa429d6b911
benangeloh <benbenprofessional@gmail.com>
2025-09-07 12:18:31 +0700

    fix: homepage navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,40 +1,31 @@
 <script>
-import Hero from '$lib/components/home/Hero.svelte'; 
 import HeroB from '$lib/components/home/HeroB.svelte';
 import LKSection from '$lib/components/home/Lk.svelte';
-import UKM from '$lib/components/home/UKM.svelte';
-import ColorPalette from '$lib/components/home/ColorPalette.svelte';
-import CarnivalTable from '$lib/components/background/CarnivalTable.svelte';
 import UkmSection from '$lib/components/home/UKMSection.svelte';
 import Faq from '$lib/components/home/Faq.svelte';
 import Swal from '$lib/components/swal/swal.svelte';
+import ColorPalette from '$lib/components/home/ColorPalette.svelte';
+import Navbar from '$lib/components/layout/Navbar.svelte';
 </script>
 
 <Swal />
+<Navbar />
 
 <main class="relative overflow-x-hidden">
-  <!-- <Hero /> -->
-  <HeroB />
-  <UkmSection />
-  <div class="relative z-10">
-    <LKSection />
-  </div>
-  <div class="z-20 h-screen sticky top-0 -mt-[100vh]">
-      <Faq />
-  </div>
-  <!-- <div class="relative z-20 h-screen sticky top-0 -mt-[100vh]">
-    <img 
-      src="/svg/home/ukm/table top.svg" 
-      alt="Table Top Decoration" 
-      class="w-full object-cover flex-shrink-0 z-30"
-    />
-    <div class="flex-grow overflow-hidden">
-      <UKM />
-    </div> 
-  </div> -->
-
-  <!-- <ColorPalette /> -->
-
+	<div id="hero">
+		<HeroB />
+	</div>
+	<div id="ukm">
+		<UkmSection />
+	</div>
+	<div class="relative z-10" id="lk">
+		<LKSection />
+	</div>
+	<div class="z-20 h-screen sticky top-0 -mt-[100vh]" id="faq">
+		<Faq />
+	</div>
+  
+  <ColorPalette />
 </main>
 
 <style>
