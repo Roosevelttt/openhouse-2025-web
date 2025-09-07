7bde66a50be585ed75fd3c7ccbcb87c50c525432
shellyOei <oeishelly15@gmail.com>
2025-09-08 02:11:53 +0700

    fix: vid and poster component's aos

--- src/lib/components/ukm/ImageFrame.svelte
+++ src/lib/components/ukm/ImageFrame.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <!-- svelte-ignore a11y_img_redundant_alt -->
-<div data-aos="fade-up" data-aos-duration="700" data-aos-offset=500 class={`relative h-auto ${className}`} bind:this={frame}>
+<div data-aos="fade-up" data-aos-duration="700" data-aos-delay="100" class={`relative h-auto ${className}`} bind:this={frame}>
   <img src="/images/ukm/photo-frame.webp" alt=""
     class="absolute top-0 left-0 z-10">
   <img src="{src}" alt=""
--- src/lib/components/ukm/Poster.svelte
+++ src/lib/components/ukm/Poster.svelte
@@ -7,5 +7,5 @@
         alt="Event Poster"
         class="w-full max-w-sm md:w-[80%] lg:w-[60%] rounded-2xl shadow-lg "
         data-aos="fade-up"
-        data-aos-offset="500"
+        data-aos-delay="400"
       />
