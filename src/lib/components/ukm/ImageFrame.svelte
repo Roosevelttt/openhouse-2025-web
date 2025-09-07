74a753c7ad4c9b3dfc217a82285aa08ae84aa686
shellyOei <oeishelly15@gmail.com>
2025-09-08 01:48:10 +0700

    fix: fixed ratio for photos

--- src/lib/components/ukm/ImageFrame.svelte
+++ src/lib/components/ukm/ImageFrame.svelte
@@ -7,11 +7,9 @@
 </script>
 
 <!-- svelte-ignore a11y_img_redundant_alt -->
-<div data-aos="fade-up" data-aos-duration="700" data-aos-offset=500 class={`${className}`} bind:this={frame}>
-  <div class="relative">
-    <img src="/images/ukm/photo-frame.webp" alt=""
-      class="absolute top-0 left-0 z-10">
-    <img src="{src}" alt=""
-      class="absolute top-0 left-1/2 -translate-x-1/2 w-[90%]">
-  </div>
+<div data-aos="fade-up" data-aos-duration="700" data-aos-offset=500 class={`relative h-auto ${className}`} bind:this={frame}>
+  <img src="/images/ukm/photo-frame.webp" alt=""
+    class="absolute top-0 left-0 z-10">
+  <img src="{src}" alt=""
+    class="absolute top-3 sm:top-[3%] left-1/2 -translate-x-1/2 aspect-[1/1] w-[90%] object-cover">
 </div>
