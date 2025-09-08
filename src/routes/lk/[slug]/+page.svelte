b08d92e5d6a249d4ab41914652a7888e398b462b
shellyOei <oeishelly15@gmail.com>
2025-09-08 16:06:16 +0700

    fix: img base

--- src/routes/lk/[slug]/+page.svelte
+++ src/routes/lk/[slug]/+page.svelte
@@ -3,7 +3,7 @@
   import Background from "$lib/components/ukm/Background.svelte";
   import Logo from "$lib/components/ukm/Logo.svelte";
   import { onMount } from "svelte";
-  import { PUBLIC_API_BASE, PUBLIC_IMAGE_BASE } from "$env/static/public";
+  import { PUBLIC_API_BASE } from "$env/static/public";
   import Subtitle from "$lib/components/ukm/Subtitle.svelte";
   import Video from "$lib/components/ukm/Video.svelte";
   import AOS from 'aos';
@@ -34,7 +34,7 @@
     if (!relativeUrl) return null;
     if (relativeUrl.startsWith("http")) return relativeUrl;
 
-    return `${PUBLIC_IMAGE_BASE}${relativeUrl}`.replace(/([^:]\/)\/+/g, "$1");
+    return `${PUBLIC_API_BASE}${relativeUrl}`.replace(/([^:]\/)\/+/g, "$1");
   }
 
   function parseImageUrls(imageUrls: string | null): string[] {
