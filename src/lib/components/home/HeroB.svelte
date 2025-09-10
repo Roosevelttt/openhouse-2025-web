c41f33bd2019fccddca126d92340f3095e39a77a
benangeloh <benbenprofessional@gmail.com>
2025-09-11 00:12:19 +0700

    chore: hero src video type

--- src/lib/components/home/HeroB.svelte
+++ src/lib/components/home/HeroB.svelte
@@ -282,7 +282,7 @@
 >
   <div class="absolute inset-0 w-full h-full">
     <video playsinline autoplay muted loop class="w-full h-full object-cover">
-      <source src="/videos/teaser-landscape.mp4" type="video/webm" />
+      <source src="/videos/teaser-landscape.mp4" type="video/mp4" />
     </video>
   </div>
 
